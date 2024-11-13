c9599771f5fbd916f8cd64df859c0a475877c505
jcllt10 <llaczajc10@gmail.com>
2024-11-13 12:21:08 -0500

    segundo commit bike-service para rest-template

--- user-service/src/main/java/com/juanca/userservice/controller/UserController.java
+++ user-service/src/main/java/com/juanca/userservice/controller/UserController.java
@@ -3,6 +3,8 @@ package com.juanca.userservice.controller;
 import com.juanca.userservice.entity.User;
 import com.juanca.userservice.service.UserService;
 import java.util.List;
+import model.Bike;
+import model.Car;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -51,6 +53,38 @@ public class UserController {
         User newUser = userService.insertUser(user);
         
         return ResponseEntity.ok(newUser);
+    }
+    
+    @GetMapping("/user/cars/{idUser}")
+    public ResponseEntity<List<Car>> listarCarsPorIdUser(@PathVariable("idUser") int idUser)
+    {
+       User user = userService.getUserById(idUser);
+       
+       if(user == null)
+          return ResponseEntity.notFound().build();
+       
+       List<Car> cars = userService.getCars(idUser);
+       
+       if(cars==null)
+           return ResponseEntity.notFound().build();
+       
+       return ResponseEntity.ok(cars);
+    }
+    
+    @GetMapping("/user/bikes/{idUser}")
+    public ResponseEntity<List<Bike>> listarBikesPorIdUser(@PathVariable("idUser") int idUser)
+    {
+       User user = userService.getUserById(idUser);
+       
+       if(user == null)
+          return ResponseEntity.notFound().build();
+       
+       List<Bike> bikes = userService.getBikes(idUser);
+       
+       if(bikes==null)
+           return ResponseEntity.notFound().build();
+       
+       return ResponseEntity.ok(bikes);
     }
     
 }
--- user-service/src/main/java/com/juanca/userservice/service/UserService.java
+++ user-service/src/main/java/com/juanca/userservice/service/UserService.java
@@ -3,14 +3,20 @@ package com.juanca.userservice.service;
 import com.juanca.userservice.entity.User;
 import com.juanca.userservice.respository.IUserRepository;
 import java.util.List;
+import model.Bike;
+import model.Car;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class UserService {
     
     @Autowired
     private IUserRepository userRepository;
+    
+    @Autowired
+    private RestTemplate restTemplate;
     
     public List<User> listUsers()
     {
@@ -25,6 +31,20 @@ public class UserService {
     public User insertUser(User newUser)
     {
       return userRepository.save(newUser);
+    }
+    
+     public List<Car> getCars(int idUser)
+    {
+      List<Car> carsByUser = restTemplate.getForObject("http://localhost:8022/api/v1/cars/" + idUser, List.class);
+      
+      return carsByUser;
+    }
+    
+    public List<Bike> getBikes(int idUser)
+    {
+      List<Bike> bikesByUser = restTemplate.getForObject("http://localhost:8023/api/v1/bikes/" + idUser, List.class);
+       
+       return bikesByUser;
     }
     
 }
